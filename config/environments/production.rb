e8177845a98df22dd31ef2da5adc8aa0cda6a90c
Weyman Fung <wfung0083@yahoo.com>
2014-04-03 14:14:09 -0700

    down

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-RolyPolly::Application.configure do
+WeymanPort::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -24,8 +24,6 @@ RolyPolly::Application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-
-  
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
